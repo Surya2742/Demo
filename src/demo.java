6c7768c9e70feb46ed865a039da1dd025dff0f99
Surya2742 <boby7724284@gmail.com>
2022-09-02 19:33:40 +0530

    fixed issue

--- src/demo.java
+++ src/demo.java
@@ -1,8 +1,8 @@
 public class demo {
     public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println("Hello");
-        System.out.println("Hello");
-        System.out.println("Hello");
+        System.out.println("Hello1");
+        System.out.println("Hello2");
+        System.out.println("Hello3");
+        System.out.println("Hello4");
     }
 }
